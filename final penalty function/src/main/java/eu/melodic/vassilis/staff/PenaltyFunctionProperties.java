2703612e133a5ce5e2d6ce0d3be05df0cee78889
ipatini <ipatini@acci.gr>
2019-09-23 15:48:16 +0200

    patini changes

--- final penalty function/src/main/java/eu/melodic/vassilis/staff/PenaltyFunctionProperties.java
+++ final penalty function/src/main/java/eu/melodic/vassilis/staff/PenaltyFunctionProperties.java
@@ -1,32 +1,34 @@
 package eu.melodic.vassilis.staff;
 
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.stereotype.Component;
-import org.springframework.validation.annotation.Validated;
-import io.github.cloudiator.rest.model.NodeCandidate;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.*;
-import org.slf4j.*;
-import java.util.HashMap;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-
-
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 
-
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 @Data
-@Configuration
+@ConfigurationProperties
 @PropertySource("file:${MELODIC_CONFIG_DIR}/eu.melodic.config.properties")
 @Slf4j
 public class PenaltyFunctionProperties {
-    private final HashMap<String,String> myconfig = new HashMap<>();
+    private final Map<String, String> startupTimes = new HashMap<>();
+
+    private final List<StateInfo> stateInfo;
+
+    @Data
+    public static class StateInfo {
+        private double cores;
+        private double memory;
+        private double storage;
+
+        public String toString() {
+            return String.format("StateInfo(cores=%d,ram=%.2f,disk=%.2f)", (int)cores, memory, storage);
+        }
+    }
 }
--- final penalty function/src/main/java/eu/melodic/vassilis/staff/PenaltyTests.java
+++ final penalty function/src/main/java/eu/melodic/vassilis/staff/PenaltyTests.java
@@ -9,109 +9,60 @@
 
 package eu.melodic.vassilis.staff;
 
-
-
-
-import io.github.cloudiator.rest.model.NodeCandidate;
-import io.github.cloudiator.rest.model.OperatingSystemFamily;
-
-
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-
-import io.github.cloudiator.rest.model.NodeCandidate;
-import java.lang.reflect.Type;
-import java.io.*;
-import java.util.*;
 import lombok.extern.slf4j.Slf4j;
-import java.util.HashMap;
-
-
-import org.apache.log4j.BasicConfigurator;
-
-import com.whalin.MemCached.MemCachedClient;
-import com.whalin.MemCached.SockIOPool;
-
-//import org.apache.commons.*; 
-import org.apache.commons.math3.stat.regression.OLSMultipleLinearRegression;
-
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-
-import java.lang.String;
-import java.lang.Object;
-import java.lang.*;
-
-
-import eu.melodic.vassilis.staff.ConfigurationElement;
-
-//import eu.melodic.cloudiator.client.model.NodeCandidate;
-import io.github.cloudiator.rest.model.NodeCandidate;
-import io.github.cloudiator.rest.model.OperatingSystemFamily;
-//import eu.melodic.vassilis.staff.PenaltyFunction.ConfigurationElement;
-
-
-//import org.apache.commons.*; 
-import org.apache.commons.math3.stat.regression.OLSMultipleLinearRegression;
-
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Collection;
+
+//import org.apache.commons.*;
+//import eu.melodic.cloudiator.client.model.NodeCandidate;
+//import eu.melodic.vassilis.staff.PenaltyFunction.ConfigurationElement;
+//import org.apache.commons.*;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 @Slf4j
 public class PenaltyTests implements CommandLineRunner {
-	@Autowired
-	private PenaltyFunction penaltyCalculator;
-	
-	public static void main(String[] args) {
+    @Autowired
+    private PenaltyFunction penaltyCalculator;
+    @Autowired
+    private PenaltyFunctionProperties properties;
+
+    public static void main(String[] args) {
         log.info("STARTING THE APPLICATION");
         SpringApplication.run(PenaltyTests.class, args);
         log.info("APPLICATION FINISHED");
     }
 
+    public void run(String... args) throws Exception {
+        Collection<ConfigurationElement> collection_1 = readConfigElementsFromFile(args[0]);
+        log.info("Collection-1:\n{}", PenaltyFunction.toString(collection_1));
 
+        Collection<ConfigurationElement> collection_2 = readConfigElementsFromFile(args[0]);
+        log.info("Collection-2:\n{}", PenaltyFunction.toString(collection_2));
 
+        double penaltyValue = penaltyCalculator.evaluatePenaltyFunction(collection_1, collection_2);
 
-	public void run(String... args) throws Exception {
-		Collection<ConfigurationElement> collection_1 = null;
-		Collection<ConfigurationElement> collection_2 = null;
-		
-		double finalll=0;
-		   
-		
-		try (Reader reader = new FileReader( args[0] )) {
-			Type listType = new TypeToken<ArrayList<ConfigurationElement>>(){}.getType();
-			collection_1 = new Gson().fromJson(reader, listType);
-			
-			log.info("Collection-1:\n{}", PenaltyFunction.toString(collection_1));
-		}
-		
-		try (Reader reader = new FileReader( args[1] )) {
-			Type listType = new TypeToken<ArrayList<ConfigurationElement>>(){}.getType();
-			collection_2 = new Gson().fromJson(reader, listType);
-			
-			log.info("Collection-2:\n{}", PenaltyFunction.toString(collection_2));
-		}
-		//PenaltyFunction penaltyCalculator = new PenaltyFunction();
-		finalll = penaltyCalculator.evaluatePenaltyFunction(collection_1, collection_2);
-		
-	    
-	
-       //normalized average startup time using max-min normalization
-		System.out.println("Normalized Average Time of VM Startup Time and Component Deployment Time :" + finalll);
-		
-			
-	
-	
-	}
-	
+        //normalized average startup time using max-min normalization
+        log.info("Normalized Average Time of VM Startup Time and Component Deployment Time : {}", penaltyValue);
+    }
+
+    protected Collection<ConfigurationElement> readConfigElementsFromFile(String fileName) throws IOException {
+        try (Reader reader = new FileReader(fileName)) {
+            Type listType = new TypeToken<ArrayList<ConfigurationElement>>() {}.getType();
+            return new Gson().fromJson(reader, listType);
+        }
+    }
 }
